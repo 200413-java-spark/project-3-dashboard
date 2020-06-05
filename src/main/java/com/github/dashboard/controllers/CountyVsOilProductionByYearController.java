bfd748a44c64f08fe170c01e5436d002e6474f1f
winkie224 <leanthony104@yahoo.com>
2020-06-05 15:53:47 -0500

    jeffsBranch

--- src/main/java/com/github/dashboard/controllers/CountyVsOilProductionByYearController.java
+++ src/main/java/com/github/dashboard/controllers/CountyVsOilProductionByYearController.java
@@ -12,29 +12,28 @@ import com.github.dashboard.services.CountyVsOilProductionByYearService;
 
 @RestController
 public class CountyVsOilProductionByYearController {
-	
+
 	@Autowired
 	private CountyVsOilProductionByYearService countyVsOilProductionByYearService;
-	
+
 	@RequestMapping("/CountyVsOilProductionByYear")
 	public List<CountyVsOilProductionByYearModel> getAllData() {
 		return countyVsOilProductionByYearService.getAllData();
 	}
-	
+
 	@RequestMapping("/CountyVsOilProductionByYear/{id}")
 	public CountyVsOilProductionByYearModel getSpecificDataById(@PathVariable int id) {
 		return countyVsOilProductionByYearService.getData(id);
 	}
-	
+
 	@RequestMapping("/CountyVsOilProductionByYear/filter/county/{county}")
-	public List<CountyVsOilProductionByYearModel> getFilteredByCounty(@PathVariable String county){
+	public List<CountyVsOilProductionByYearModel> getFilteredByCounty(@PathVariable String county) {
 		return countyVsOilProductionByYearService.getFilteredCounty(county);
-		
-	}
-	
-	@RequestMapping("/CountyVsOilProductionByYear/filter/year/{year}")
-	public List<CountyVsOilProductionByYearModel> getFilteredByYear(@PathVariable int year){
-		return countyVsOilProductionByYearService.getFilteredYear(year);
+
 	}
 
+	@RequestMapping("/CountyVsOilProductionByYear/filter/year/{year}")
+	public List<CountyVsOilProductionByYearModel> getFilteredByYear(@PathVariable int year) {
+		return countyVsOilProductionByYearService.getFilteredYear(year);
+	}
 }
