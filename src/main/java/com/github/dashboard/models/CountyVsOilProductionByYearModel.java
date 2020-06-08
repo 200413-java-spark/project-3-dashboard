e50d65b5170d3392668d97f11b3dc47e5a094ba7
unknown <maginajeffrey@gmail.com>
2020-06-07 22:23:16 -0400

    refactored code to be more generic and refactored using jpa repo tools

--- src/main/java/com/github/dashboard/models/CountyVsOilProductionByYearModel.java
+++ src/main/java/com/github/dashboard/models/CountyVsOilProductionByYearModel.java
@@ -1,40 +1,23 @@
 package com.github.dashboard.models;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.NamedNativeQueries;
-import javax.persistence.NamedNativeQuery;
 
 @Entity(name = "CountyVsOilProductionByYear")
-
-@NamedNativeQueries({
-		@NamedNativeQuery(name = "getByCounty", query = "Select * From County_Vs_Oil_Production_By_Year "
-				+ "WHERE county LIKE ?", resultClass = CountyVsOilProductionByYearModel.class),
-
-		@NamedNativeQuery(name = "getByYear", query = "Select * From County_Vs_Oil_Production_By_Year "
-				+ "WHERE year=?", resultClass = CountyVsOilProductionByYearModel.class) })
-
 public class CountyVsOilProductionByYearModel {
-
 	@Id
 	private int id;
 
-	@Column(name = "county")
 	private String county;
-
-	private double oil_production;
-
-	@Column(name = "year")
+	private double oilProduction;
 	private int year;
 
 	public CountyVsOilProductionByYearModel() {
-
 	}
 
 	public CountyVsOilProductionByYearModel(String county, double oil_production, int year) {
 		this.county = county;
-		this.oil_production = oil_production;
+		this.oilProduction = oil_production;
 		this.year = year;
 	}
 
@@ -47,11 +30,11 @@ public class CountyVsOilProductionByYearModel {
 	}
 
 	public double getOil_production() {
-		return oil_production;
+		return oilProduction;
 	}
 
 	public void setOil_production(double oil_production) {
-		this.oil_production = oil_production;
+		this.oilProduction = oil_production;
 	}
 
 	public int getYear() {
@@ -61,5 +44,4 @@ public class CountyVsOilProductionByYearModel {
 	public void setYear(int year) {
 		this.year = year;
 	}
-
 }
