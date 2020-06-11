db3c9ee594068fdd21f73f1fe94138f36d319467
b154810 <brandoncnelsen@gmail.com>
2020-06-11 18:42:03 -0400

    new schema

--- schema.sql
+++ schema.sql
@@ -6,26 +6,93 @@ CREATE TABLE county_vs_oil_production_by_year(
         year int
 );
 
-Insert Into county_vs_oil_production_by_year (county, oil_production, year) 
-Values ('Chautauqua', 8645, 2004);
+insert into county_vs_oil_production_by_year (county, oil_production, year)
+values ('Cattaraugus', 4500, 2002);
 
 Insert Into county_vs_oil_production_by_year (county, oil_production, year)
-Values ('Chautauqua', 4529, 2003);
+Values ('Chautauqua', 1700, 2002);
 
 Insert Into county_vs_oil_production_by_year (county, oil_production, year)
 Values ('Cattaraugus', 4528, 2003);
 
+Insert Into county_vs_oil_production_by_year (county, oil_production, year)
+Values ('Chautauqua', 2200, 2003);
+Insert Into county_vs_oil_production_by_year (county, oil_production, year)
+Values ('Chautauqua', 3100, 2003);
+
+Insert Into county_vs_oil_production_by_year (county, oil_production, year)
+Values ('Cattaraugus', 4000, 2004);
+
+Insert Into county_vs_oil_production_by_year (county, oil_production, year)
+Values ('Chautauqua', 4500, 2004);
+
+Insert Into county_vs_oil_production_by_year (county, oil_production, year)
+Values ('Cattaraugus', 3200, 2005);
+
+Insert Into county_vs_oil_production_by_year (county, oil_production, year) 
+Values ('Chautauqua', 8645, 2005);
+
+Insert Into county_vs_oil_production_by_year (county, oil_production, year)
+Values ('Cattaraugus', 2600, 2006);
+
+Insert Into county_vs_oil_production_by_year (county, oil_production, year) 
+Values ('Chautauqua', 7300, 2006);
+
+Insert Into county_vs_oil_production_by_year (county, oil_production, year)
+Values ('Cattaraugus', 2000, 2007);
+Insert Into county_vs_oil_production_by_year (county, oil_production, year)
+Values ('Cattaraugus', 3000, 2007);
+
+Insert Into county_vs_oil_production_by_year (county, oil_production, year) 
+Values ('Chautauqua', 6500, 2007);
+
 insert into county_vs_oil_production_by_year (county, oil_production, year)
-values ('Cattaraugus', 4500, 2002);
+values ('Steuben', 1000, 2007);
+
+Insert Into county_vs_oil_production_by_year (county, oil_production, year)
+Values ('Cattaraugus', 3200, 2008);
+
+Insert Into county_vs_oil_production_by_year (county, oil_production, year) 
+Values ('Chautauqua', 5000, 2008);
+
+insert into county_vs_oil_production_by_year (county, oil_production, year)
+values ('Steuben', 3000, 2008);
+
+Insert Into county_vs_oil_production_by_year (county, oil_production, year)
+Values ('Cattaraugus', 3600, 2009);
+
+Insert Into county_vs_oil_production_by_year (county, oil_production, year) 
+Values ('Chautauqua', 4500, 2009);
+
+insert into county_vs_oil_production_by_year (county, oil_production, year)
+values ('Steuben', 3000, 2009);
+
+Insert Into county_vs_oil_production_by_year (county, oil_production, year)
+Values ('Cattaraugus', 4500, 2010);
+
+Insert Into county_vs_oil_production_by_year (county, oil_production, year) 
+Values ('Chautauqua', 3600, 2010);
+
+insert into county_vs_oil_production_by_year (county, oil_production, year)
+values ('Steuben', 5000, 2010);
+insert into county_vs_oil_production_by_year (county, oil_production, year)
+values ('Steuben', 6000, 2010);
+
+insert into county_vs_oil_production_by_year (county, oil_production, year)
+values ('Chautauqua', 3530, 2011);
+
+Insert Into county_vs_oil_production_by_year (county, oil_production, year) 
+Values ('Chautauqua', 2400, 2011);
+
+insert into county_vs_oil_production_by_year (county, oil_production, year)
+values ('Steuben', 8000, 2011);
 
 insert into county_vs_oil_production_by_year (county, oil_production, year)
 values ('Chautauqua', 4269, 2012);
 
 insert into county_vs_oil_production_by_year (county, oil_production, year)
-values ('Chautauqua', 3530, 2011);
+values ('Steuben', 9000, 2012);
 
 insert into county_vs_oil_production_by_year (county, oil_production, year)
-values ('Steuben', 465, 2017);
+values ('Steuben', 7500, 2013);
 
-insert into county_vs_oil_production_by_year (county, oil_production, year)
-values ('Steuben', 490, 2017);
