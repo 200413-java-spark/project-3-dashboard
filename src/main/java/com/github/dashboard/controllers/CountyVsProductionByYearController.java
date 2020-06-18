d7782b1d1d4bf9cc21c8e00fca1b63e2e2f69db0
winkie224 <leanthony104@yahoo.com>
2020-06-17 21:25:45 -0500

    x future

--- src/main/java/com/github/dashboard/controllers/CountyVsProductionByYearController.java
+++ src/main/java/com/github/dashboard/controllers/CountyVsProductionByYearController.java
@@ -3,6 +3,7 @@ package com.github.dashboard.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import com.github.dashboard.models.CountyVsProductionByYearModel;
 import com.github.dashboard.services.CountyVsProductionByYearService;
 
 @RestController
+@CrossOrigin
 public class CountyVsProductionByYearController {
 
 	@Autowired
